45a57a9cd844140306840e051aea06f7fa416feb
rjcjunior <rjcjunior@hotmail.com>
2017-02-03 06:55:07 -0200

    TREFEITO

--- TRE.sql
+++ TRE.sql
@@ -61,6 +61,62 @@ CREATE TABLE Voto (
 
 
 --Popular tabela
+INSERT INTO localidade (nome, cep) VALUES ('niteroi','34740012');
+INSERT INTO localidade (nome, cep) VALUES ('sao goncalo','34740013');
+INSERT INTO localidade (nome, cep) VALUES ('rio de janeiro','34740014');
+
+INSERT INTO Zonaeleitoral(nome,localidade_fk) VALUES ('UFF-Praia vermelha',1);
+INSERT INTO Zonaeleitoral(nome,localidade_fk) VALUES ('Unidos de portugal',2);
+INSERT INTO Zonaeleitoral(nome,localidade_fk) VALUES ('UFRJ-Praia vermelha',3);
+
+INSERT INTO Partidopolitico(nome,codpartido) VALUES ('PMDB',1);
+INSERT INTO Partidopolitico(nome,codpartido) VALUES ('PT',2);
+INSERT INTO Partidopolitico(nome,codpartido) VALUES ('PSOL',3);
+INSERT INTO Partidopolitico(nome,codpartido) VALUES ('PSDB',4);
+
+INSERT INTO Secao(zona_fk) VALUES (1);
+INSERT INTO Secao(zona_fk) VALUES (1);
+INSERT INTO Secao(zona_fk) VALUES (2);
+INSERT INTO Secao(zona_fk) VALUES (2);
+INSERT INTO Secao(zona_fk) VALUES (3);
+INSERT INTO Secao(zona_fk) VALUES (3);
+
+INSERT INTO Candidato(nome,codcand,localidade_fk,partido_fk) VALUES ('zé do gás',10,1,1);
+INSERT INTO Candidato(nome,codcand,localidade_fk,partido_fk) VALUES ('zé do taxi',11,1,2);
+INSERT INTO Candidato(nome,codcand,localidade_fk,partido_fk) VALUES ('zé do mototaxi',12,1,3);
+INSERT INTO Candidato(nome,codcand,localidade_fk,partido_fk) VALUES ('zé da escola',10,2,1);
+INSERT INTO Candidato(nome,codcand,localidade_fk,partido_fk) VALUES ('zé do uber',22,2,2);
+INSERT INTO Candidato(nome,codcand,localidade_fk,partido_fk) VALUES ('zé da van',23,2,3);
+INSERT INTO Candidato(nome,codcand,localidade_fk,partido_fk) VALUES ('zé da borracharia',30,3,1);
+INSERT INTO Candidato(nome,codcand,localidade_fk,partido_fk) VALUES ('zé da piscina',30,3,2);
+INSERT INTO Candidato(nome,codcand,localidade_fk,partido_fk) VALUES ('zé do Vasco',30,3,3);
+
+
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('zé da escola','15758866445',1);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Ricardo','15758866442',1);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Carol','15758866443',2);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Bia','15758866446',2);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Laura','15758866447',3);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Fabiana','15758866448',3);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Ronnie','12758866446',4);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Ragnar','13758866447',4);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Michele','24758866448',5);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Fernando','34758866448',5);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Rodrigo','44758866448',6);
+INSERT INTO Eleitor(nome,cpf,secao_fk) VALUES ('Diogo','54758866448',6);
+
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (1,1);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (1,1);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (2,2);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (2,1);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (4,4);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (4,4);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (4,4);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (3,5);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (5,9);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (5,9);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (6,9);
+INSERT INTO Voto(secao_fk,candidato_fk) VALUES (6,9);
 
 
 
@@ -68,10 +124,9 @@ CREATE TABLE Voto (
 
 --CANDIDATOS COM NENHUM VOTO
 SELECT c.nome
-FROM candidato c, voto v
-where c.candidato_id = v.candidato_fk
+FROM candidato c
+where c.candidato_id not in (select v.candidato_fk from voto v)
 group by c.nome
-having count(v.candidato_fk)=0;
 
 --CANDIDATOS MAIS VOTADO
 SELECT c.nome
